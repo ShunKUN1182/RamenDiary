26d2aac2b46b334b0ba9d041d6317f88cd1df52a
ShunKUN1182 <2250378@ecc.ac.jp>
2025-12-07 20:47:08 +0900

    key取得

--- js/new_vlog.js
+++ js/new_vlog.js
@@ -1,1 +1,10 @@
-console.log("foo");
+const textData = document.querySelector("#text_data");
+const numberData = document.querySelector("#number_data");
+const ramenTaste = document.querySelector("#ramen_taste");
+const ramenJudge = document.querySelector("#ramen_judge");
+const submitBtn = document.querySelector("#submit_btn");
+
+const supabaseUrl = "https://gcflelpasndsydtugmja.supabase.co";
+const supabaseKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdjZmxlbHBhc25kc3lkdHVnbWphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUxMDMwMTMsImV4cCI6MjA4MDY3OTAxM30.YlQYvRSJ8-7uJMb7OsoB8HbuQGT7hSZ-NJIW6Q2Xllk";
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
