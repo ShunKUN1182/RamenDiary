093783fc223c5e02cf24b38a3ccf18c04ba15e61
ShunKUN1182 <2250378@ecc.ac.jp>
2025-12-07 23:51:12 +0900

    簡単に機能を作成

--- js/view_vlog.js
+++ js/view_vlog.js
@@ -3,16 +3,27 @@ const supabaseKey =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdjZmxlbHBhc25kc3lkdHVnbWphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUxMDMwMTMsImV4cCI6MjA4MDY3OTAxM30.YlQYvRSJ8-7uJMb7OsoB8HbuQGT7hSZ-NJIW6Q2Xllk";
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
-const ramen_view = document.querySelector(".ramen_view");
+const ramenView = document.querySelector(".ramen_view");
 
 async function loadData() {
     const { data, error } = await supabase.from("ramen_data").select("*");
     console.log("data:", data);
     console.log("error:", error);
-    // for (let i = 0; i < data.length; i++) {
-    //     list.insertAdjacentHTML("beforeend", `<li> ${i + 1} : ${data[i].title}</li>`);
-    //     list.insertAdjacentHTML("beforeend", `<img src="${data[i].image_url}">`);
-    // }
+    for (let i = 0; i < data.length; i++) {
+        ramenView.insertAdjacentHTML(
+            "afterbegin",
+            `
+                      <div class="ramen_data">
+                        <img src="${data[i].image_url}" alt="" width="100" height="100"/>
+                        <div>
+                            <h2>${data[i].ramen_name}</h2>
+                            <h3>評価：${data[i].ramen_judge}</h3>
+                            <h3>${data[i].ramen_price}円分食べました</h3>
+                        </div>
+                    </div>
+                  `
+        );
+    }
 }
 
 loadData();
