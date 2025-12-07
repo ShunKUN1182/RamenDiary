f12da9a71876110f0a1920a9b8d901d526136b8c
ShunKUN1182 <2250378@ecc.ac.jp>
2025-12-07 21:12:57 +0900

    アップロードのテスト完了

--- js/new_vlog.js
+++ js/new_vlog.js
@@ -8,3 +8,20 @@ const supabaseUrl = "https://gcflelpasndsydtugmja.supabase.co";
 const supabaseKey =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdjZmxlbHBhc25kc3lkdHVnbWphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUxMDMwMTMsImV4cCI6MjA4MDY3OTAxM30.YlQYvRSJ8-7uJMb7OsoB8HbuQGT7hSZ-NJIW6Q2Xllk";
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+
+// async function loadData() {
+//     const { data, error } = await supabase.from("ramen_data").select("*");
+//     console.log("data:", data);
+//     console.log("error:", error);
+// }
+// loadData();
+
+async function insertTest() {
+    const { error } = await supabase.from("ramen_data").insert({ ramen_name: textData.value });
+    // console.log("無事成功しました");
+    // console.log(error);
+}
+
+submitBtn.addEventListener("click", () => {
+    insertTest();
+});
