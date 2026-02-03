377930b0d4a87891afc309dbe5ee6e4ce799bdd2
ShunKUN1182 <2250378@ecc.ac.jp>
2026-02-03 15:42:29 +0900

    画像のパスを変数化

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 import { supabase } from "./supabase.js";
 
+const bucketName = "ramen_images";
+const submitBtn = document.querySelector("#submit_btn");
+const ramenImage = document.querySelector("#addPicture");
+
 async function loadData() {
     const { data, error } = await supabase.from("ramen_data").select("*");
     console.log("data:", data);
@@ -7,3 +11,16 @@ async function loadData() {
 }
 
 loadData();
+
+submitBtn.addEventListener("click", async () => {
+    let file = ramenImage.files[0];
+    if (!file) {
+        alert("画像を選択してください");
+        // ここは将来的にない場合はそのまま通して画像をnotImageに差し替える
+        return;
+    }
+    console.dir(file);
+    const reFile = file.name.split(".").pop();
+    const fileName = `${crypto.randomUUID()}.${reFile}`;
+    console.log(fileName);
+});
