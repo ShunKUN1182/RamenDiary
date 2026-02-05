a631de744baad6dda59e4bb5c644a8204376d8ed
ShunKUN1182 <2250378@ecc.ac.jp>
2026-02-05 10:37:32 +0900

    合計金額を表示

--- js/graph.js
+++ js/graph.js
@@ -1,13 +1,20 @@
 import { supabase } from "./supabase.js";
 
+const ramenCount = document.querySelector("#ramenCount");
+const priceOutput = document.querySelector("#totalPrice");
+
 async function totalPrice() {
     const { data, error } = await supabase.from("ramen_data").select("ramen_price");
     console.log(data, error);
+    ramenCount.innerHTML = data.length;
     let priceBox = [];
     for (let i = 0; i < data.length; i++) {
         priceBox.push(Number(data[i].ramen_price));
     }
-    console.log(priceBox);
+    const totalPrice = priceBox.reduce((current, currentVal) => {
+        return current + currentVal;
+    });
+    priceOutput.innerHTML = totalPrice;
 }
 
 totalPrice();
