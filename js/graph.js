081af592a594277f39db31f55d2e463088d1822a
ShunKUN1182 <2250378@ecc.ac.jp>
2026-02-06 10:49:54 +0900

    グラフ作成（仮）

--- js/graph.js
+++ js/graph.js
@@ -2,19 +2,76 @@ import { supabase } from "./supabase.js";
 
 const ramenCount = document.querySelector("#ramenCount");
 const priceOutput = document.querySelector("#totalPrice");
+const ctx = document.getElementById("tasteChart");
 
 async function totalPrice() {
     const { data, error } = await supabase.from("ramen_data").select("ramen_price");
-    console.log(data, error);
+
+    if (error) {
+        console.log(error);
+        return;
+    }
+
     ramenCount.innerHTML = data.length;
+
     let priceBox = [];
     for (let i = 0; i < data.length; i++) {
         priceBox.push(Number(data[i].ramen_price));
     }
+
     const totalPrice = priceBox.reduce((current, currentVal) => {
         return current + currentVal;
-    });
+    }, 0);
+
     priceOutput.innerHTML = totalPrice;
 }
 
+async function tasteGraph() {
+    const { data, error } = await supabase.from("ramen_data").select("ramen_taste");
+
+    if (error) {
+        console.log(error);
+        return;
+    }
+
+    // tasteを集計する
+    const tasteCount = {};
+
+    data.forEach((item) => {
+        const taste = item.ramen_taste;
+
+        if (tasteCount[taste]) {
+            tasteCount[taste]++;
+        } else {
+            tasteCount[taste] = 1;
+        }
+    });
+
+    const labels = Object.keys(tasteCount);
+    const values = Object.values(tasteCount);
+
+    new Chart(ctx, {
+        type: "doughnut", // 円グラフなら "pie" でもOK
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                    label: "ラーメンの味",
+                    data: values,
+                    borderWidth: 2,
+                },
+            ],
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: "bottom",
+                },
+            },
+        },
+    });
+}
+
 totalPrice();
+tasteGraph();
